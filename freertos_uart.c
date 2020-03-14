16bae190074a3366a7ff235921d2924a13a62f4d
David Flores Ruiz <ie717807@iteso.mx>
2020-03-14 16:35:02 -0600

    Prueba de la UART

--- freertos_uart.c
+++ freertos_uart.c
@@ -1,37 +1,18 @@
 /*
  * Copyright 2016-2020 NXP
  * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
- 
-/**
- * @file    freertos_uart.c
- * @brief   Application entry point.
+
+/*
+ * @file:			Drivers with FreeRTOS.c
+ * @brief:   		Tarea4_UART_I2C_GPIO_in_FreeRTOS_OS_for_FRDM-K66F
+ *
+ * @company:			  ITESO
+ * @Engineer Team:	 D.F.R. / R.G.P.
+ * @contact:		ie717807@iteso.mx
+ * @contact:		ie706818@iteso.mx
  */
+
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
@@ -39,6 +20,7 @@
 #include "clock_config.h"
 #include "MK66F18.h"
 #include "fsl_debug_console.h"
+
 /* TODO: insert other include files here. */
 #include "FreeRTOS.h"
 #include "task.h"
@@ -46,7 +28,54 @@
 #include "rtos_I2C.h"
 
 /* TODO: insert other definitions and declarations here. */
-void uart_echo_task(void * args)
+
+
+/* TODO: insert the prototypes of functions here. */
+void uart_echo_task(void *args);
+//void I2C_echo_task(void *args);
+
+int main(void) {
+
+	/* Init board hardware. */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+	BOARD_InitBootPeripherals();
+	/* Init FSL debug console. */
+	BOARD_InitDebugConsole();
+
+	PRINTF("Hello World\n");
+
+	/* xTaskCreate uses:
+	 * 		- Pointer to the function that implements the task.
+	 * 		- Text name for the task.  This is to facilitate debugging only.
+	 * 		- Stack depth - most small microcontrollers will use much less stack than this.
+	 * 		- Task parameter, Pasamos una cadena de texto para DEBUG
+	 *
+	 * 		- Priority
+	 * 		- We are not using the task handle.
+	 * 		*/
+
+	xTaskCreate(uart_echo_task, "uart_echo_task", 110, NULL,
+			configMAX_PRIORITIES, NULL);
+
+//	xTaskCreate(uart_echo_task, "I2C_echo_task",  110, NULL,
+//			configMAX_PRIORITIES, NULL);
+
+	vTaskStartScheduler();
+
+	while (1) {
+		/* Your Application never comes here :) */
+		__asm volatile ("nop");
+	}
+	return 0;
+}
+
+
+
+/* TODO: insert the body (implementation) of the functions here. */
+
+
+void uart_echo_task(void *args)
 {
 	rtos_uart_config_t config;
 	config.baudrate = 115200;
@@ -57,41 +86,9 @@ void uart_echo_task(void * args)
 	config.port = rtos_uart_portB;
 	rtos_uart_init(config);
 	uint8_t data;
-	for(;;)
-	{
+
+	for (;;) {
 		rtos_uart_receive(rtos_uart0, &data, 1);
 		rtos_uart_send(rtos_uart0, &data, 1);
 	}
-}
-void I2C_echo_task(void * args)
-{
-}
-/*
- * @brief   Application entry point.
- */
-int main(void) {
-
-  	/* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
-  	/* Init FSL debug console. */
-    BOARD_InitDebugConsole();
-
-    PRINTF("Hello World\n");
-
-    xTaskCreate(uart_echo_task, "uart_echo_task", 110, NULL, 1, NULL);
-    xTaskCreate(uart_echo_task, "I2C_echo_task" , 110, NULL, 2, NULL);
-    vTaskStartScheduler();
-
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
 }
--- rtos_I2C.c
+++ rtos_I2C.c
@@ -14,13 +14,13 @@
 #include "semphr.h"
 
 #define BAUD_RATE (9600U)
-static rtos_I2C_handle_t uart_handles[NUMBER_OF_I2C_PORTS] = {0};
+//static rtos_I2C_handle_t uart_handles[NUMBER_OF_I2C_PORTS] = {0};
 
-rtos_I2C_flag_t rtos_i2c_init(rtos_I2C_config_t config){
-	rtos_I2C_flag_t retval = I2C_uart_fail;
-	i2c_master_config_t config;
-	if(config.I2c_number < NUMBER_OF_I2C_PORTS){
-
-	}
-}
+//rtos_I2C_flag_t rtos_i2c_init(rtos_I2C_config_t config){
+//	rtos_I2C_flag_t retval = I2C_uart_fail;
+//	i2c_master_config_t config;
+//	if(config.I2c_number < NUMBER_OF_I2C_PORTS){
+//
+//	}
+//}
 
